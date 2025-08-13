4fdb4093fa7ee9f21714b2e77d75069fb43399b8
menaBM <mena.bousfieldmilton@sky.uk>
2025-08-13 08:33:27 +0100

    translate drivers routes to supababse functions

--- backend/supabase/functions/drivers/index.ts
+++ backend/supabase/functions/drivers/index.ts
@@ -9,7 +9,12 @@ const baseUrl = "https://f1connectapi.vercel.app/api"
 
 router.get('/drivers', async (request, response) => {
   try {
-    const result = await fetch(`${baseUrl}/drivers`)
+    let name = request.query.name
+    if (name) {
+      // the api only accepts either first name or last name as the query parameter
+      name = name.split(" ")[0] // so if multiple words have been entered only use the first
+    }
+    const result = await fetch(`${baseUrl}/drivers${name ? `/search?q=${name}` : ""}`)
     const { drivers } = await result.json()
     if (drivers.length < 1) {
       response.sendStatus(404)
@@ -21,15 +26,48 @@ router.get('/drivers', async (request, response) => {
 });
 
 router.get('/drivers/featured', async (request, response) => {
+  try {
+    const result = await fetch(`${baseUrl}/current/drivers`)
+    const { drivers } = await result.json()
 
+    if (drivers.length < 1) {
+      response.sendStatus(404)
+    }
+
+    const index = Math.round(Math.random() * 100) % drivers.length
+    response.status(200).send(drivers[index])
+  } catch (error) {
+    response.status(500).send(error)
+  }
 });
 
 router.get('/drivers/current', async (request, response) => {
+  try {
+    const result = await fetch(`${baseUrl}/current/drivers`)
+    const { drivers } = await result.json()
 
+    if (drivers.length < 1) {
+        response.sendStatus(404)
+    }
+    response.status(200).send(drivers)
+  } catch (error) {
+    response.status(500).send(error)
+  }
 });
 
 router.get('/drivers/id/:id', async (request, response) => {
+   try {
+      const driverId = request.params.id
+      const result = await fetch(`${baseUrl}/drivers/${driverId}`)
+      const { driver } = await result.json()
 
+      if (!driver) {
+          response.sendStatus(404)
+      }
+      response.status(200).send(driver)
+    } catch (error) {
+      response.status(500).send(error)
+    }
 });
 
 router.listen(3000, () => {})
