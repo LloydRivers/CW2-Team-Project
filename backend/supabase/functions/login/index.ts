b7b1422d30b353a39df177b90cc2382d68022ac4
menaBM <mena.bousfieldmilton@sky.uk>
2025-08-21 08:59:53 +0100

    add test login route

---
+++ backend/supabase/functions/login/index.ts
@@ -0,0 +1,45 @@
+import "jsr:@supabase/functions-js/edge-runtime.d.ts"
+import express from 'npm:express'
+import {createClient} from "npm:@supabase/supabase-js@2";
+
+const router = express();
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
+
+const supabase = createClient(Deno.env.get("SUPABASE_URL"), Deno.env.get("SUPABASE_SERVICE_ROLE_KEY"))
+
+// provided for testing purposes
+
+router.post('/login', async (request, response) => {
+  try {
+    const {email, password} = request.body
+    const result = await supabase.auth.signInWithPassword({email, password})
+
+    if (result.error) {
+      response.status(result.status).send(result.error)
+      return
+		}
+
+    response.send(result)
+  } catch (error) {
+    response.status(500).send(error)
+  }
+});
+
+router.post('/login/signup', async (request, response) => {
+  try {
+    const {email, password} = request.body
+    const result = await supabase.auth.signUp({email, password})
+    
+    if (result.error) {
+      response.status(result.status).send(result.error)
+			return
+		}
+
+    response.send(result)
+  } catch (error) {
+    response.status(500).send(error)
+  }
+});
+
+router.listen(3000, () => {})
